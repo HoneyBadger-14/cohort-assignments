a2a018006b9ebe1745039e937b1ca0eda8448b4f
Puneet Gaddi <puneetgaddi14@gmail.com>
2024-01-17 13:13:00 +0530

    Solving the middleware of node express

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -10,15 +10,49 @@ let requestCount = 0;
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
 
-app.get('/user', function(req, res) {
+//Application level middleware
+//Handles all requests
+//These must be placed above the endpoints 
+
+// app.use((req, res, next) => {
+//   requestCount += 1;
+//   next();
+// });
+
+// //Handles all the methods of request to this path [GET, PUT, POST]
+// app.use('/user', (req, res, next) => {
+//   console.log('Request type:', req.method);
+//   next();
+// });
+
+// //Handles GET requests
+// app.get('/user', (req, res, next) => {
+//   console.log('Specifically GET middleware');
+//   next();
+// });
+
+// app.get('/requestCount', (req, res, next) => {
+//   console.log('Specifically requestCount middleware with app.get');
+//   next();
+// });
+
+//Declaring the function array of middlewares within the handler 
+function incrementCount(req, res, next) {
+  requestCount += 1;
+  console.log('Incrementing the count in middleware');
+  next();
+};
+const middleStuff = [incrementCount];
+
+app.get('/user', middleStuff, function (req, res) {
   res.status(200).json({ name: 'john' });
 });
 
-app.post('/user', function(req, res) {
+app.post('/user', middleStuff, function (req, res) {
   res.status(200).json({ msg: 'created dummy user' });
 });
 
-app.get('/requestCount', function(req, res) {
+app.get('/requestCount', middleStuff, function (req, res) {
   res.status(200).json({ requestCount });
 });
 
